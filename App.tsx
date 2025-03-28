8f64e86b23e559f6eb3bbbb52d02b7888a91b7f7
Ana Julia Oliveira da Silva <contatoajuliaos@gmail.com>
2025-03-28 20:28:17 -0300

    Inicialização: App React Native Marcação de Consultas EC

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.texto}
+      >Engenharia da Computação</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff1ce',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto: {
+    fontSize: 30,
+    color: '#000000',
+    fontWeight: 'bold',
+  }
 });
